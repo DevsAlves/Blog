ce8bb9e0b3596bcfc3c729325f2ad61651840100
Guilherme_Alves <Alves@gmail.com>
2024-10-29 20:45:39 -0300

    Script: Organizando a parte de pesquisa

--- src/script.js
+++ src/script.js
@@ -1,14 +1,21 @@
 const form = document.getElementById("search-blog");
 const search = document.getElementById("search-bar");
 
-form.addEventListener("click", () => {
+form.addEventListener("mouseover", () => {
     search.classList.add("form");
 
-    setTimeout( () => {
-        search.classList.replace("form" , "searc-bar");
-    }, 1500);
+    // setTimeout( () => {
+    //     search.classList.replace("form" , "searc-bar");
+    // }, 1500);
 });
 
+form.addEventListener("mouseout", () => {
+    search.classList.replace("form" , "searc-bar");
+}); 
+
+
+
 // Parte de Busca da main 
+const query = document.getElementById("posts-container");
 
 
